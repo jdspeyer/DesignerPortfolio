0fd4e6731209bd0132f51cf82e686ee0b85dc4be
Jake Speyer <jspeyer@globebiomedical.com>
2023-11-08 02:06:28 -0800

    got rid of cursor

--- javascript/scripts/listeners.js
+++ javascript/scripts/listeners.js
@@ -23,6 +23,7 @@ document.getElementById('all_projects_button').onclick = function () { MENU_MANA
 
 if (Engine.isTouchDevice()) {
   document.getElementById('model').classList.add('ignore_input')
+  document.getElementById('cursor').style.display = 'none'
 }
 
 /// Initialize the Engine.
